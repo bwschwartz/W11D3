33bbd34f2c9bcf26c729868170822fcb5bb16d1c
Frederick <fred.chien3@gmail.com>
2022-09-28 14:15:49 -0400

    complete store setup

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReudcers, applyMiddleware, compose} from "redux";
+import { createStore, combineReducers, applyMiddleware, compose} from "redux";
 
 
 const rootReducer = combineReducers({
